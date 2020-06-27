0ad971e84e6d2fa7fde44129404548a247938db5
Nick Lemoing <nicklemoign@Nicks-MacBook-Pro.local>
2020-06-27 16:00:00 -0400

    Add Strategy base class that implements random choice

---
+++ cribbage/strategy.py
@@ -0,0 +1,35 @@
+from typing import List, Tuple
+from random import choice, sample
+
+class Strategy:
+    """
+    Strategy is an abstract base class for implementing cribbage strategies.
+    It exposes two methods. The first, chooseHand, takes a hand of 6 cards and
+    returns a hand of four as well as the two discarded cards. The second
+    method is the pegging function, peg, which takes a hand and the sequence of
+    previously played cards and returns a card to play next. 
+    Besides that, anything goes!
+    """
+    def chooseHand(self, options: List[int]) -> List[int]:
+        """
+        chooseHand expects an array of integers with length 6 that represents
+        a set of 6 cards from which to choose a hand. It should return a list
+        of 4 cards that are kept. The list should be made up of the same
+        elements as were in the original list, but this can't be enforced here
+        so we'll also do sanity checks when running simulations. 
+        The default is to choose a random partition.
+        """
+        return sample(options, 4)
+
+    def peg(self, hand: List[int], previousCards: List[List[Tuple[int]]]) -> int:
+        """
+        peg takes a hand and a set of previous cards that were played. Peg will
+        only be called when a move is possible (e.g. a go is not forced). hand
+        only contains cards that haven't been played yet, so whatever peg
+        returns should be part of hand (we'll check!). previousCards is a list
+        of previously played cards, grouped by 31s (each time 31 is reached, a
+        new list is added). Each tuple is a pair containing a card and a player
+        number (0 if the opponent, 1 otherwise).
+        The default is to choose a random element of hand.
+        """
+        return choice(hand)
